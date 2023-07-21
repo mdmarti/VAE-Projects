d75d5251ce0e74e0aa266a3759c862c25428f639
mdmarti <miles.martinez@duke.edu>
2023-07-21 11:48:31 -0400

    moving plotting to plot.py

--- models/toy_models.py
+++ models/toy_models.py
@@ -7,7 +7,7 @@ from train import train
 from data import *
 from tqdm import tqdm
 import seaborn as sns
-import scipy
+from plot import plotFlows2dLorenz,plotSamples1d,plotSamples3d
 
 def generate_ndim_benes(n=100,d = 20,T=100,dt=1):
 
@@ -94,104 +94,6 @@ def generate_stochastic_lorenz(n=100,T=100,dt=1,coeffs=[10,28,8/3,0.15,0.15,0.15
 	allPaths = [p + 0.01*np.random.randn(*p.shape) for p in allPaths]
 	#allPaths = [p[::5] for p in allPaths]
 	return allPaths
-
-def plotSamples3d(true,generated,n=100):
-
-	fig = plt.figure()
-	ax1 = fig.add_subplot(121,projection='3d')
-	ax2 = fig.add_subplot(122,projection='3d')
-	order = np.random.choice(len(true),100,replace=False)
-	for o in order:
-		t = true[o]
-		ax1.plot(t[:,0],t[:,1],t[:,2])
-	ax1.set_xticks([])
-	ax1.set_yticks([])
-	ax1.set_zticks([])
-		
-	order = np.random.choice(len(true),100,replace=False)
-
-	for o in order:
-		g = generated[o]
-		ax2.plot(g[:,0],g[:,1],g[:,2])
-	ax2.set_xticks([])
-	ax2.set_yticks([])
-	ax2.set_zticks([])
-	plt.show()
-
-def plotSamples1d(true,generated):
-	fig = plt.figure()
-	ax1 = fig.add_subplot(121)
-	ax2 = fig.add_subplot(122)
-	order = np.random.choice(len(true),100,replace=False)
-	true_lims = (np.amin(true)-0.1,np.amax(true)+0.1)
-	for o in order:
-		t = true[o].squeeze()
-		time = list(range(1,len(t)+1))
-		ax1.plot(time,t)
-	ax1.set_xticks([])
-	ax1.set_ylim(true_lims)
-	#ax1.set_yticks([])
-
-	
-	order = np.random.choice(len(generated),100,replace=False)
-
-	for o in order:
-		g = generated[o].squeeze()
-		time = list(range(1,len(g)+1))
-		ax2.plot(time,g)
-	ax2.set_xticks([])
-	ax2.set_ylim(true_lims)
-	#ax1.set_yticks([])
-	plt.show()
-
-
-def sde_gif(data):
-
-	fig = plt.figure()
-	if data[0].shape[-1] == 2:
-		ax = fig.add_subplot(111)
-		for traj in data:
-
-			ax.plot(traj[:,0],traj[:,1])
-			ax.set_xlabel("dim 1")
-			ax.set_ylabel("dim 2")
-
-		plt.savefig("testingonetwo.png")
-	else:
-		
-		for ii,traj in enumerate(data):
-			mins = np.amin(traj,axis=0)
-			maxs = np.amax(traj,axis=0)
-			ax = fig.add_subplot(111,projection='3d')
-
-			ax.set_xlim([mins[0],maxs[0]])
-			ax.set_ylim([mins[1],maxs[1]])
-			ax.set_zlim([mins[2],maxs[2]])
-
-			line, = ax.plot([],[])
-			ax.view_init(azim=180)
-
-			def makeAni1(frame):
-				if frame < traj.shape[0]:
-					line.set_data_3d(traj[:frame+1,0],traj[:frame+1,1],traj[:frame+1,2])
-				ax.view_init(azim=180 + frame/6)
-				ax.set_xlabel("dim 1")
-				ax.set_ylabel("dim 2")
-				ax.set_zlabel("dim 3")
-				return line,
-		
-			print("animating 1")
-			anim= animation.FuncAnimation(fig,makeAni1,frames=3000,interval=20,blit=True)
-			anim.save(f'changeAcrossTime{ii}.gif', writer = 'ffmpeg', fps = 50)
-			plt.close('all')
-			
-
-
-		#plt.savefig("testingonetwothree.png")
-
-def plotFlows2dLorenz(params,model):
-
-	pass
 
 if __name__ == '__main__':
 
