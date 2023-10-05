fixing variance scaling, adding projection

models/data.py
@@ -106,10 +106,11 @@ def generate_2d_swirls(n=100,T=1,dt=0.001,theta=10,mu=1.01,sigma=0.5,x0=np.array
 	lam_fnc = lambda x: (torch.diag_embed(1/(sigma * x))).detach().cpu().numpy()[:,inds[0],inds[1]]
 	return allPaths,(mu_fnc,d_fnc),(eta_fnc,lam_fnc)
 
-def generate_stochastic_lorenz(n=100,T=100,dt=1,coeffs=[10,28,8/3,0.15,0.15,0.15],zscore=False):
+def generate_stochastic_lorenz(n=100,T=1,dt=0.001,coeffs=[10,28,8/3,0.15,0.15,0.15],zscore=False):
 
 	sigma,rho,beta = coeffs[0],coeffs[1],coeffs[2]
-	A1,A2,A3 = coeffs[0],coeffs[1],coeffs[2]
+	A = np.array([coeffs[0],coeffs[1],coeffs[2]])
+	Sig = np.eye(3) * A
 	t = np.arange(0,T,dt)
 	
 	allPaths = [ ]
@@ -127,13 +128,13 @@ def generate_stochastic_lorenz(n=100,T=100,dt=1,coeffs=[10,28,8/3,0.15,0.15,0.15
 			y = prev[1]
 			z = prev[2]
 
-			sample_dW = np.sqrt(dt) * np.random.randn(3)
-			dx = sigma * (y - x)*dt +A1 * sample_dW[0]
-			dy = (x * (rho - z) - y)*dt +A2 * sample_dW[1]
-			dz = (x*y  - beta*z)*dt +A3 * sample_dW[2]
+			sample_dW = A @ (np.sqrt(dt) * np.random.randn(3))
+			dx = sigma * (y - x)*dt #+ sample_dW[0]
+			dy = (x * (rho - z) - y)*dt #+ sample_dW[1]
+			dz = (x*y  - beta*z)*dt #+ sample_dW[2]
 
 			#assert (abs(dx) < 200) & (abs(dy) < 200) & (abs(dz)<200),print(dx,dy,dz,ii,jj)
-			xx.append(xx[jj-1] + np.hstack([dx,dy,dz]))
+			xx.append(xx[jj-1] + np.hstack([dx,dy,dz]) + sample_dW)
 
 		xx = np.vstack(xx)
 		assert xx.shape[0] == (len(t) + 1), print(xx.shape)

models/encoding.py
@@ -1,7 +1,7 @@
 from abc import ABC 
 from abc import abstractmethod
 from typing import Optional, Union, Tuple, Any
-
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -97,9 +97,63 @@ class ConvEncoder(linearEncoder):
 		YOU BREAK IT
 
 		YOU BUY IT BADABING BADABOOM
+		Just use the version from AVA
+
+		WIP
 		"""
 		self.data_dim = data_dim
 		self.latent_dim = latent_dim
 		self.bias= has_bias
 		F = [nn.Conv2d(in_channels=1,out_channels=4)]
+
+
+
+def _softmax(x:np.array):
+
+	m = np.amax (x,axis=1)
+	e_x = np.exp(x - m)
+	return e_x/np.sum(e_x,axis=1) 
+
+class projection:
+
+	"""
+	
+	we just need one class for this
+	"""
+
+	def __init__(self,origDim,newDim, projType='linear') -> None:
+		self.d1 = origDim
+		self.d2 = newDim
+		self.projType=projType 
+		self.W = np.random.randn(origDim,newDim)
+		if projType == 'linear':
+			
+
+			self.projection = lambda x: x @ self.W 
 		
+		elif projType == 'softmax':
+
+			self.projection = lambda x: _softmax(x @ self.W)
+
+		else:
+			print('Method must be softmax or linear')
+			raise NotImplementedError
+
+	
+	def project(
+		self,
+		data: np.array,
+	 ) -> np.array:
+		
+		r"""
+		Return batch projectiojns.
+		
+
+		Args: 
+			data: what we are embedding in a higher-d space
+			
+		"""
+
+		return self.projection(data)
+	
+

models/latent_sde.py
@@ -417,7 +417,7 @@ class nonlinearLatentSDE(latentSDE,nn.Module):
 		### estimate cholesky factor ####
 		L = torch.zeros(zt1.shape[0],self.dim,self.dim).to(self.device)
 		D = torch.exp(self.D(zt1))
-		L[:,self.chol_inds[0],self.chol_inds[1]] = D 
+		L[:,self.chol_inds[0],self.chol_inds[1]] = D* torch.sqrt(dt) 
 		####### invert cholesky factor #######
 		eyes = torch.eye(self.dim).view(1,self.dim,self.dim).repeat(zt1.shape[0],1,1).to(self.device)
 		invChol = torch.linalg.solve_triangular(L,eyes,upper=False)
@@ -474,7 +474,7 @@ class nonlinearLatentSDE(latentSDE,nn.Module):
 			true_p1.append(self.true1(batch[0]))
 			true_p2.append(self.true2(batch[0]))
 
-			if (ii == batchInd) & (self.epoch % 5 ==0) & self.plotDists:
+			if (ii == batchInd) & (self.epoch % 100 ==0) & self.plotDists:
 				
 				self._add_quiver(batch[0].detach().cpu().numpy(),mu.detach().cpu().numpy(),self.true1(batch[0]),self.p1name,'Train')
 		epoch_mus = np.vstack(epoch_mus)
@@ -494,7 +494,36 @@ class nonlinearLatentSDE(latentSDE,nn.Module):
 		self.epoch += 1
 	
 		return epoch_loss,optimizer
-	
+
+	def val_epoch(self,loader):
+
+		self.eval()
+		with torch.no_grad():
+			epoch_loss = 0.
+			epoch_mus = []
+			epoch_Ds = []
+			true_p1 = []
+			true_p2 = []
+			batchInd = np.random.choice(len(loader))
+			for ii,batch in enumerate(loader):
+				loss,mu,d = self.forward(batch)
+				
+				epoch_loss += loss.item()
+
+				epoch_mus.append(mu.detach().cpu().numpy())
+				epoch_Ds.append(d.detach().cpu().numpy())
+				true_p1.append(self.true1(batch[0]))
+				true_p2.append(self.true2(batch[0]))
+				
+		epoch_mus = np.vstack(epoch_mus)
+		epoch_Ds = np.vstack(epoch_Ds)
+		true_p1 = np.vstack(true_p1)
+		true_p2 = np.vstack(true_p2)
+
+		self.writer.add_scalar('Test/loss',epoch_loss/len(loader),self.epoch)
+
+		return epoch_loss
+
 	def test_epoch(self,loader):
 
 		self.eval()
@@ -716,14 +745,14 @@ class nonlinearLatentSDENatParams(nonlinearLatentSDE,nn.Module):
 		### estimate cholesky factor ####
 		D = torch.exp(self.D(zt1))
 		L = torch.zeros(zt1.shape[0],self.dim,self.dim).to(self.device)
-		L[:,self.chol_inds[0],self.chol_inds[1]] = D 
+		L[:,self.chol_inds[0],self.chol_inds[1]] = D /torch.sqrt(dt)
 		#### Precision ###########
-		precision = L @ L.transpose(-2,-1) /dt
+		precision = L @ L.transpose(-2,-1)#/dt
 		####### invert cholesky factor #######
 		eyes = torch.eye(self.dim).view(1,self.dim,self.dim).repeat(zt1.shape[0],1,1).to(self.device)
 		LInv = torch.linalg.solve_triangular(L,eyes,upper=False)
 		###### covariance #####
-		cov = LInv.transpose(-2,-1) @ LInv * dt
+		cov = LInv.transpose(-2,-1) @ LInv# * dt
 		
 		##### calculate loss ###################
 		c = -self.dim/2 * np.log(2*torch.pi)

models/plot.py
@@ -7,7 +7,7 @@ def plotSamples3d(true,generated,n=100):
 	fig = plt.figure()
 	ax1 = fig.add_subplot(121,projection='3d')
 	ax2 = fig.add_subplot(122,projection='3d')
-	order = np.random.choice(len(true),100,replace=False)
+	order = np.random.choice(len(true),n,replace=False)
 	for o in order:
 		t = true[o]
 		ax1.plot(t[:,0],t[:,1],t[:,2])
@@ -15,7 +15,7 @@ def plotSamples3d(true,generated,n=100):
 	ax1.set_yticks([])
 	ax1.set_zticks([])
 		
-	order = np.random.choice(len(true),100,replace=False)
+	order = np.random.choice(len(true),n,replace=False)
 
 	for o in order:
 		g = generated[o]
@@ -25,11 +25,11 @@ def plotSamples3d(true,generated,n=100):
 	ax2.set_zticks([])
 	plt.show()
 
-def plotSamples1d(true,generated):
+def plotSamples1d(true,generated,n=100):
 	fig = plt.figure()
 	ax1 = fig.add_subplot(121)
 	ax2 = fig.add_subplot(122)
-	order = np.random.choice(len(true),100,replace=False)
+	order = np.random.choice(len(true),n,replace=False)
 	true_lims = (np.amin(true)-0.1,np.amax(true)+0.1)
 	for o in order:
 		t = true[o].squeeze()
@@ -41,7 +41,7 @@ def plotSamples1d(true,generated):
 	#ax1.set_yticks([])
 
 	
-	order = np.random.choice(len(generated),100,replace=False)
+	order = np.random.choice(len(generated),n,replace=False)
 
 	for o in order:
 		g = generated[o].squeeze()
@@ -53,12 +53,12 @@ def plotSamples1d(true,generated):
 	#ax1.set_yticks([])
 	plt.show()
 
-def plotSamples2d(true: list, samples: list) -> None:
+def plotSamples2d(true: list, samples: list,n=100) -> None:
 
 	fig = plt.figure()
 	ax1 = fig.add_subplot(121)
 	ax2 = fig.add_subplot(122)
-	order = np.random.choice(len(true),100,replace=False)
+	order = np.random.choice(len(true),n,replace=False)
 	mins, maxs = np.amin(np.vstack(true),axis=0),np.amax(np.vstack(true),axis=0)
 	
 	true_xlims = (mins[0]-0.1,maxs[0]+0.1)
@@ -75,7 +75,7 @@ def plotSamples2d(true: list, samples: list) -> None:
 	#ax1.set_yticks([])
 
 	
-	order = np.random.choice(len(samples),100,replace=False)
+	order = np.random.choice(len(samples),n,replace=False)
 	mins, maxs = np.amin(np.vstack(samples),axis=0),np.amax(np.vstack(samples),axis=0)
 	
 	true_xlims = (mins[0]-0.1,maxs[0]+0.1)