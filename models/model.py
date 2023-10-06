291b941d8e299217586128e3a64cc8b4364ecb6a
mdmarti <miles.martinez@duke.edu>
2023-10-06 13:29:52 -0400

    getting model to run

--- models/model.py
+++ models/model.py
@@ -8,27 +8,31 @@ class EmbeddingSDE(nn.Module):
 	def __init__(self,dataDim,latentDim,encoding,latentSDE,
 				 device='cuda'):
 
-
+		super(EmbeddingSDE,self).__init__()
 		self.sde = latentSDE
 		self.encoder =encoding 
 		self.dataDim=dataDim
 		self.latentDim=latentDim 
+		self.device=device
 		self.to(self.device)
 
 
 	def encode_trajectory(self,data):
 
-		return self.encoder.encode(data)
+		return self.encoder.forward(data.to(self.device)).detach().cpu().numpy()
 	
 	def generate_trajectory(self,init_conditions,T,dt):
 
+		init_conditions = init_conditions.to(self.device)
 		if init_conditions.shape[-1] == self.dataDim:
 
-			z0 = self.encoder.encode(init_conditions)
+			with torch.no_grad():
+				z0 = self.encoder.forward(init_conditions)
+			z0 = z0.squeeze().detach().cpu().numpy()
 
 		elif init_conditions.shape[-1] == self.latentDim:
 
-			z0 = init_conditions
+			z0 = init_conditions.detach().cpu().numpy()
 
 		else:
 
@@ -74,28 +78,28 @@ class EmbeddingSDE(nn.Module):
 			epoch_mus.append(mu.detach().cpu().numpy())
 			epoch_Ds.append(d.detach().cpu().numpy())
 			
-			true_p1.append(self.sde.true1(batch[0]))
-			true_p2.append(self.sde.true2(batch[0]))
+			true_p1.append(self.sde.true1(z1))
+			true_p2.append(self.sde.true2(z1))
 
 
-			if (ii == batchInd) & (self.epoch % 100 ==0) & self.sde.plotDists:
+			if (ii == batchInd) & (self.sde.epoch % 100 ==0) & self.sde.plotDists:
 				
-				self.sde._add_quiver(batch[0].detach().cpu().numpy(),mu.detach().cpu().numpy(),self.true1(batch[0]),self.p1name,'Train')
+				self.sde._add_quiver(z1.detach().cpu().numpy(),mu.detach().cpu().numpy(),self.sde.true1(z1),self.sde.p1name,'Train')
 		epoch_mus = np.vstack(epoch_mus)
 		epoch_Ds = np.vstack(epoch_Ds)
 		true_p1 = np.vstack(true_p1)
 		true_p2 = np.vstack(true_p2)
 
-		self.sde.writer.add_scalar('Train/loss',epoch_loss/len(loader),self.epoch)
-		if self.plotDists & (self.epoch % 50 == 0):
+		self.sde.writer.add_scalar('Train/loss',epoch_loss/len(loader),self.sde.epoch)
+		if self.sde.plotDists & (self.sde.epoch % 50 == 0):
 			
-			for d in range(self.dim):
+			for d in range(self.sde.dim):
 				
-				self.sde._add_dist_figure(epoch_mus[:,d],true_p1[:,d],self.p1name,d+1,'Train')
-				#self.writer.add_scalars(f'Train/{self.p2name} dim {d+1}',{'estimated':epoch_sigs[d],'true':self.true2[d]},self.epoch)
-			for d in range(self.n_entries):
-				self.sde._add_dist_figure(epoch_Ds[:,d],true_p2[:,d],self.p2name,d+1,'Train')
-		self.epoch += 1
+				self.sde._add_dist_figure(epoch_mus[:,d],true_p1[:,d],self.sde.p1name,d+1,'Train')
+				#self.sde.writer.add_scalars(f'Train/{self.sde.p2name} dim {d+1}',{'estimated':epoch_sigs[d],'true':self.sde.true2[d]},self.sde.epoch)
+			for d in range(self.sde.n_entries):
+				self.sde._add_dist_figure(epoch_Ds[:,d],true_p2[:,d],self.sde.p2name,d+1,'Train')
+		self.sde.epoch += 1
 	
 		return epoch_loss,optimizer
 	
@@ -111,21 +115,21 @@ class EmbeddingSDE(nn.Module):
 			true_p2 = []
 			batchInd = np.random.choice(len(loader))
 			for ii,batch in enumerate(loader):
-				loss,mu,d = self.forward(batch)
+				loss,z1,z2,mu,d = self.forward(batch)
 				
 				epoch_loss += loss.item()
 
 				epoch_mus.append(mu.detach().cpu().numpy())
 				epoch_Ds.append(d.detach().cpu().numpy())
-				true_p1.append(self.true1(batch[0]))
-				true_p2.append(self.true2(batch[0]))
+				true_p1.append(self.sde.true1(z1))
+				true_p2.append(self.sde.true2(z1))
 				
 		epoch_mus = np.vstack(epoch_mus)
 		epoch_Ds = np.vstack(epoch_Ds)
 		true_p1 = np.vstack(true_p1)
 		true_p2 = np.vstack(true_p2)
 
-		self.writer.add_scalar('Test/loss',epoch_loss/len(loader),self.epoch)
+		self.sde.writer.add_scalar('Test/loss',epoch_loss/len(loader),self.sde.epoch)
 
 		return epoch_loss
 
@@ -140,46 +144,46 @@ class EmbeddingSDE(nn.Module):
 			true_p2 = []
 			batchInd = np.random.choice(len(loader))
 			for ii,batch in enumerate(loader):
-				loss,mu,d = self.forward(batch)
+				loss,z1,z2,mu,d = self.forward(batch)
 				
 				epoch_loss += loss.item()
 
 				epoch_mus.append(mu.detach().cpu().numpy())
 				epoch_Ds.append(d.detach().cpu().numpy())
-				true_p1.append(self.true1(batch[0]))
-				true_p2.append(self.true2(batch[0]))
-				if (ii == batchInd) & self.plotDists:
-					self._add_quiver(batch[0].detach().cpu().numpy(),mu.detach().cpu().numpy(),self.true1(batch[0]),self.p1name,'Test')
+				true_p1.append(self.sde.true1(z1))
+				true_p2.append(self.sde.true2(z1))
+				if (ii == batchInd) & self.sde.plotDists:
+					self.sde._add_quiver(z1.detach().cpu().numpy(),mu.detach().cpu().numpy(),self.sde.true1(z1),self.sde.p1name,'Test')
 
 		epoch_mus = np.vstack(epoch_mus)
 		epoch_Ds = np.vstack(epoch_Ds)
 		true_p1 = np.vstack(true_p1)
 		true_p2 = np.vstack(true_p2)
 
-		self.writer.add_scalar('Test/loss',epoch_loss/len(loader),self.epoch)
-		if self.plotDists:
-			for d in range(self.dim):
-				self._add_dist_figure(epoch_mus[:,d],true_p1[:,d],self.p1name,d+1,'Test')
-				#self.writer.add_scalars(f'Train/{self.p2name} dim {d+1}',{'estimated':epoch_sigs[d],'true':self.true2[d]},self.epoch)
-			for d in range(self.n_entries):
-				self._add_dist_figure(epoch_Ds[:,d],true_p2[:,d],self.p2name,d+1,'Test')
+		self.sde.writer.add_scalar('Test/loss',epoch_loss/len(loader),self.sde.epoch)
+		if self.sde.plotDists:
+			for d in range(self.sde.dim):
+				self.sde._add_dist_figure(epoch_mus[:,d],true_p1[:,d],self.sde.p1name,d+1,'Test')
+				#self.sde.writer.add_scalars(f'Train/{self.sde.p2name} dim {d+1}',{'estimated':epoch_sigs[d],'true':self.sde.true2[d]},self.sde.epoch)
+			for d in range(self.sde.n_entries):
+				self.sde._add_dist_figure(epoch_Ds[:,d],true_p2[:,d],self.sde.p2name,d+1,'Test')
 		"""
-		for d in range(self.dim):
-			if self.plotDists:
-				self.writer.add_scalars(f'Test/{self.p1name} dim {d+1}',{'estimated':epoch_mus[d],'true':self.true1[d]},self.epoch)
-				self.writer.add_scalars(f'Test/{self.p2name} dim {d+1}',{'estimated':epoch_sigs[d],'true':self.true2[d]},self.epoch)
+		for d in range(self.sde.dim):
+			if self.sde.plotDists:
+				self.sde.writer.add_scalars(f'Test/{self.sde.p1name} dim {d+1}',{'estimated':epoch_mus[d],'true':self.sde.true1[d]},self.sde.epoch)
+				self.sde.writer.add_scalars(f'Test/{self.sde.p2name} dim {d+1}',{'estimated':epoch_sigs[d],'true':self.sde.true2[d]},self.sde.epoch)
 			else:
-				self.writer.add_scalar(f'Test/{self.p1name} dim {d+1}',epoch_mus[d],self.epoch)
-				self.writer.add_scalar(f'Test/{self.p2name} dim {d+1}',epoch_sigs[d],self.epoch)
+				self.sde.writer.add_scalar(f'Test/{self.sde.p1name} dim {d+1}',epoch_mus[d],self.sde.epoch)
+				self.sde.writer.add_scalar(f'Test/{self.sde.p2name} dim {d+1}',epoch_sigs[d],self.sde.epoch)
 		"""
 		return epoch_loss
 	
 	def save(self):
 
 		sd = self.state_dict()
-		fn = os.path.join(self.save_dir,f'checkpoint_{self.epoch}.tar')
+		fn = os.path.join(self.sde.save_dir,f'checkpoint_{self.sde.epoch}.tar')
 		torch.save({
-			'epoch': self.epoch,
+			'epoch': self.sde.epoch,
 			'model_state_dict':sd
 		},fn)
 		return 
@@ -187,6 +191,6 @@ class EmbeddingSDE(nn.Module):
 	def load(self,fn):
 		print(f"Loading state from: {fn}")
 		check = torch.load(fn)#,map_location=self.device)
-		self.epoch = check['epoch']
+		self.sde.epoch = check['epoch']
 		self.load_state_dict(check['model_state_dict'])
 		return
