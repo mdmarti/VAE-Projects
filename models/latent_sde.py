d4fdb0e5bda6fcb19578d1e7572886631225d819
mdmarti <miles.martinez@duke.edu>
2023-11-11 23:44:07 -0500

    what is going on.

--- models/latent_sde.py
+++ models/latent_sde.py
@@ -471,10 +471,10 @@ class nonlinearLatentSDE(latentSDE,nn.Module):
 		#cov = L @ L.transpose(-2,-1) 
 		##### calculate loss ###################
 
-		const = -self.dim/2 * np.log(2*torch.pi)
+		const = -self.dim/2 * torch.log(2*torch.pi)
 		## |LL^T| = |L| |L^T| = |L|^2 =  (\prod_i L_ii)^2
 		## LOG |LL^T| = 2(\SUM_I  LOG L_II
-		t1 = -2*torch.diagonal(L,dim1=-2,dim2=-1).log().sum(axis=-1) 
+		t1 = -torch.logdet(precision)#-2*torch.diagonal(L,dim1=-2,dim2=-1).log().sum(axis=-1) 
 		assert len(t1.shape) == 1, print(t1.shape)
 		t2 = (dzTrue.transpose(-2,-1) @ precision @ dzTrue).squeeze()
 		assert len(t2.shape)==1,print(t2.shape)
