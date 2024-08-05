0d3a7f20d1b3b738e7735c25d699c89761317001
mdmarti <miles.martinez@duke.edu>
2024-08-05 14:35:25 -0400

    everything is now a class

--- models/toy_data_flow.py
+++ models/toy_data_flow.py
@@ -4,6 +4,7 @@ import numpy as np
 import warnings
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from scipy.ndimage import gaussian_filter
 from abc import ABC,abstractmethod
 
 EPSILON = 1e-12
@@ -32,24 +33,28 @@ class ToyData():
 
 	@abstractmethod
 	def f(self,x,t):
-		pass
+		raise NotImplementedError
 
 	@abstractmethod
 	def g(self,x,t):
-		pass
+		raise NotImplementedError
 
 	@abstractmethod
 	def dW(self,dt):
-		pass 
-
-	@abstractmethod
-	def dx(self,x,t,dt,sigma):
-		pass 
+		raise NotImplementedError 
 
 	@abstractmethod
 	def init_conditions(self):
-		pass
+		raise NotImplementedError
 
+	def dx(self,x,t,dt,sigma):
+
+		fx = self.f(x,t)
+		gx = self.g(x,t,sigma)
+		dw = self.dW(dt)
+
+		return fx * dt + gx @ dw
+	
 	def generate(self,n,T,dt,sigma):
 
 		trajectories = []
@@ -75,7 +80,7 @@ class ToyData():
 	
 class Vanderpol(ToyData):
 
-	def __init__(self,coeffs,seed=1234):
+	def __init__(self,coeffs=[2,15],seed=1234):
 
 		super(Vanderpol,self).__init__()
 		self.rho,self.tau = coeffs
@@ -86,26 +91,18 @@ class Vanderpol(ToyData):
 		dx2 = self.tau/self.rho * x[0]
 		return np.hstack([dx1,dx2])
 	def g(self,x,t,sigma):
-		return sigma*x
+		return sigma*np.eye(2)
 	
 	def dW(self,dt):
-		return self.gen.multivariate_normal(mean=np.zeros((2,)),scale=np.sqrt(dt)*np.eye)
-	
-	def dx(self,x,t,dt,sigma):
+		return self.gen.multivariate_normal(mean=np.zeros((2,)),cov=dt*np.eye(2))
 
-		fx = self.f(x,t)
-		gx = self.g(x,t,sigma)
-		dw = self.dW(dt)
-
-		return fx * dt + gx @ dw
-	
 	def init_conditions(self):
 
 		return self.gen.multivariate_normal(mean=[1,1],cov=np.eye(2)*0.03)
 	
 class DoubleCircles(ToyData):
 
-	def __init__(self,coeffs,seed=1234):
+	def __init__(self,coeffs=[3.5,4,2*np.pi],seed=1234):
 
 		super(DoubleCircles,self).__init__()
 
@@ -141,13 +138,13 @@ class DoubleCircles(ToyData):
 		return r,theta
 	
 	def init_conditions(self):
-		r0 = self.r0 
+		r0 = self.r0 + self.gen.normal(loc=0,scale=0.1)
 		t0 = self.gen.uniform(0,2*np.pi)
 
-		return self.gen.multivariate_normal(mean=np.zeros((2,)),cov = self.a*np.eye(2))#self._polar_to_cartesian(r0,t0)
+		return self._polar_to_cartesian(r0,t0)
 
 	def dx(self,x,t,dt,sigma):
-		#print(x.shape)
+		
 		r,theta = self._cartesian_to_polar(x)
 		dr = self.fr(r,t)
 		dtheta = self.ft(theta,r,t)
@@ -157,515 +154,192 @@ class DoubleCircles(ToyData):
 		newX = self._polar_to_cartesian(r,theta)
 		dw_xy = self.g(x,t,sigma) @ self.dW(dt)
 		xy2 = newX + dw_xy
-		#print(xy2.shape)
+		
 		return xy2 - x
 
+class Lorenz63(ToyData):
 
+	def __init__(self,coeffs=[10,28,8/3],seed=1234):
 
-def generate_vanderpol(n=100,T = 1, dt=0.001,rho=2,tau=15,sigma=0.25,x0=np.array([1,1])):
+		super(Lorenz63,self).__init__()
+		self.sigma,self.rho,self.beta = coeffs
+		self.gen = np.random.default_rng(seed=seed)
 
-	allPaths=[]
-	t = np.arange(0,T,dt)
-	#print("not adding noise")
-	
-	def f(x,t):
-		dx1 = rho * tau * (x[0] - x[0]**3/3 - x[1])
-		dx2 = tau/rho * x[0]
-		return np.hstack([dx1,dx2])
-	def g(x,t):
-		return sigma*x
-	
-	def dW(dt):
-		return np.random.normal(loc=np.zeros((2,)),scale=np.sqrt(dt))
-	
-	for ii in range(n):
-
-		xnot = x0 + 0.03**2 * np.random.randn(2)
-		xx = [xnot]
-
-		for jj in range(1,len(t)+1):
-
-			x = xx[jj-1]
-			tt = t[jj-1]
-			xx.append(x + f(x,tt)*dt + g(x,tt)*dW(dt))
-
-		xx = np.vstack(xx)
-		assert xx.shape[0] == (len(t) + 1), print(xx.shape)
+	def f(self,x,t):
 		
-		allPaths.append(xx)
-
-	return allPaths
-
-def generate_2d_swirls(n=100,T=1,dt=0.001,
-					   omegas=(np.pi/5,-np.pi/2.5),bounds=((0.5,1),(1.25,1.5)),sigma=0.,
-					   seed=1040):
-	"""
-	Makes the circles dataset (https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html#sklearn.datasets.make_circles),
-	but as a dynamical system. 
-	Takes as arguments:
-		n: number of trajectories to make
-		T: integration time
-		dt: integration timestep
-		omegas: rotation speed for (inner,outer) circles
-		sigma: sd of noise added to rotation (default zero)
-		bounds: define width of each circle. arranged as:
-				((inner circle inner bound, inner circle outer bound),
-				 (outer circle inner bound, outer circle outer bound))
-
-	Returns:
-		trajectories:
-			list of n np.ndarrays of size (T/dt)x2, each element corresponding to a trajectory
-	"""    
-
-	trajectories=[]
-	t = np.arange(0,T,dt)
-	gen = np.random.default_rng(seed=seed)
-	
-	
-	def f(theta,t,omega):
-		dtheta = omega*dt
-		return dtheta
-		
-	def g(x,t):
-		return sigma*x
-	
-	def dW(dt):
-		return gen.multivariate_normal(mean=np.zeros((2,)),cov=dt*np.eye(2))
-
-	
-	for ii in range(n):
-		
-		circle = gen.binomial(1,0.5)
-		r = gen.uniform(bounds[circle][0],bounds[circle][1])
-		theta = gen.uniform(0,2*np.pi)
-		omega = omegas[circle]
-
-		xnot = np.hstack([r*np.cos(theta), r*np.sin(theta)])
-		xx = [xnot]
-		
-		for jj in range(1,len(t)+1):
-			x = xx[jj-1]
-			tt = t[jj-1]
-			dtheta = f(theta,tt,omega)
-			theta += dtheta
-			xx2 = np.hstack([r*np.cos(theta), r*np.sin(theta)])
-			xx.append(xx2 + g(x,tt)*dW(dt))
-		xx = np.vstack(xx)
-		
-		trajectories.append(xx)
-	
-	
-	return trajectories
-
-def generate_radial_odes(n=100,T=1,dt=0.001,
-						coeffs=[1.5,2,np.pi/4],sigma=0.,
-						seed=1040):
-	"""
-	Makes the circles dataset (https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html#sklearn.datasets.make_circles),
-	but as a dynamical system, but with a double well potential on the radius 
-	Takes as arguments:
-	n: number of trajectories to make
-	T: integration time
-	dt: integration timestep
-	coeffs:
-		center of double well, weight on quadratic term, rotation per second, weight on dR
-
-	Returns:
-	trajectories:
-	list of n np.ndarrays of size (T/dt)x2, each element corresponding to a trajectory
-	"""    
-
-	trajectories=[]
-	t = np.arange(0,T,dt)
-	gen = np.random.default_rng(seed=seed)
-	
-	r0,a,omega = coeffs
-
-	def ft(theta,t,omega):
-		#dtheta = omega
-		return omega
-	
-	def fr(r,t):
-		
-		# potential function: (r - r0)^4 - a(r - r0)^2
-		return -(4 * (r - r0)**3 - 2*a * (r - r0))
-
-	def g(x,t):
-		return sigma*np.eye(2)
-
-	def dW(dt):
-		return gen.multivariate_normal(mean=np.zeros((2,)),cov = dt*np.eye(2))
-
-
-	for ii in range(n):
-
-		xnot = gen.multivariate_normal(mean=np.zeros((2,)),cov = a*np.eye(2))
-	 
-		theta = np.arctan2(xnot[1],xnot[0])
-		r = np.linalg.norm(xnot)
-		xx = [xnot]
-		for jj in range(1,len(t)+1):
-			x = xx[jj-1]
-			tt = t[jj-1]
-			if r < r0:
-				dtheta = ft(theta,tt,omega)
-			else:
-				dtheta = ft(theta,tt,-omega)
-			dr = fr(r,tt)
-			theta += dtheta*dt
-			r += dr*dt
-			r = max(0,r)
-			xy = np.array([r*np.cos(theta),r*np.sin(theta)])
-			dw_xy = g(xy,tt) @ dW(dt)
-			xy2 = xy + dw_xy
-
-			theta = np.arctan2(xy2[1],xy2[0])
-			r = np.linalg.norm(xy2)
-			xx2 = np.hstack(xy2)
-			xx.append(xx2)
-		xx = np.vstack(xx)
-
-		trajectories.append(xx)
-
-
-	return trajectories
-
-def generate_stochastic_lorenz63(n=100,T=1,dt=0.001,coeffs=[10,28,8/3,0.,0.,0.],seed=1024):
-
-	"""
-	Makes a stochastic lorenz attractor 
-	Takes as arguments:
-		n: number of trajectories to make
-		T: integration time
-		dt: integration timestep
-		coeffs:
-			[sigma,rho,beta,sd1,sd2,sd3]
-		where sd1-3 are dimension-wise sds of added noise
-		sigma: sd of noise added to rotation (default zero)
-		seed: seed for rng
-	
-	Returns:
-		trajectories:
-			list of n np.ndarrays of size (T/dt)x2, each element corresponding to a trajectory
-	"""    
-	
-	sigma,rho,beta = coeffs[0],coeffs[1],coeffs[2]
-	A = np.array([coeffs[3],coeffs[4],coeffs[5]])
-	Sig = np.eye(3) * A
-	t = np.arange(0,T,dt)
-	gen = np.random.default_rng(seed=seed)
-	
-	allPaths = [ ]
-	
-	def f(x,t):
-		dx = sigma * (x[1] - x[0]) #+ sample_dW[0]
-		dy = (x[0] * (rho - x[2]) - x[1]) #+ sample_dW[1]
-		dz = (x[0]*x[1]  - beta*x[2]) #+ sample_dW[2]
+		dx = self.sigma * (x[1] - x[0]) #+ sample_dW[0]
+		dy = (x[0] * (self.rho - x[2]) - x[1]) #+ sample_dW[1]
+		dz = (x[0]*x[1]  - self.beta*x[2]) #+ sample_dW[2]
 		return np.hstack([dx,dy,dz])
 	
-	def g(x,t):
-		return Sig
+	def g(self,x,t,sigma):
+		return sigma*np.eye(3)
 	
-	def dW(dt):
-		return gen.multivariate_normal(mean=np.zeros((3,)),cov=np.eye(3)*dt)
+	def dW(self,dt):
+		return self.gen.multivariate_normal(mean=np.zeros((3,)),cov=dt*np.eye(3))
 	
-	for ii in range(n):
-	
-		
-		xnot = gen.multivariate_normal(mean=np.zeros((3,)),cov=np.eye(3))
-		
-		
-		xx = [xnot]
-		for jj in range(1,len(t)+1):
-			
-			x = xx[jj-1]
-			tt = t[jj-1]
-			xx.append(x + f(x,tt)*dt + g(x,tt)@dW(dt))
-		
-		xx = np.vstack(xx)
-		assert xx.shape[0] == (len(t) + 1), print(xx.shape)
-		
-		allPaths.append(xx)
-	
-	
-	return allPaths
+	def init_conditions(self):
 
-def generate_stochastic_lorenz96(n=100,T=1,d=10,dt=0.001,coeffs=[8,0],seed=1024):
+		return self.gen.multivariate_normal(mean=np.zeros((3,)),cov=np.eye(3))
 
-	"""
-	Makes a stochastic high-d lorenz attractor 
-	Takes as arguments:
-		n: number of trajectories to make
-		T: integration time
-		dt: integration timestep
-		coeffs:
-			[sigma,rho,beta,sd1,sd2,sd3]
-		where sd1-3 are dimension-wise sds of added noise
-		sigma: sd of noise added to rotation (default zero)
-		seed: seed for rng
-	
-	Returns:
-		trajectories:
-			list of n np.ndarrays of size (T/dt)x2, each element corresponding to a trajectory
-	"""    
-	
-	F,sigma = coeffs[0],coeffs[1]
-	t = np.arange(0,T,dt)
-	gen = np.random.default_rng(seed=seed)
-	
-	allPaths = [ ]
-	
-	def f(x,t):
-		dx = np.zeros(d)
+class Lorenz96(ToyData):
+
+	def __init__(self,coeffs=[8],d=10,seed=1234):
+
+		super(Lorenz96,self).__init__()
+		self.F = coeffs
+		self.d = d
+		self.gen = np.random.default_rng(seed=seed)
+
+	def f(self,x,t):
+		dx = np.zeros(self.d)
 		# Loops over indices (with operations and Python underflow indexing handling edge cases)
-		for i in range(d):
-			dx[i] = (x[(i + 1) % d] - x[i - 2]) * x[i - 1] - x[i] + F
+		for i in range(self.d):
+			dx[i] = (x[(i + 1) % self.d] - x[i - 2]) * x[i - 1] - x[i] + self.F
 		return dx
 	
-	def g(x,t):
-		return np.eye(d)*sigma
+	def g(self,x,t,sigma):
+		return np.eye(self.d)*sigma
 	
-	def dW(dt):
-		return gen.multivariate_normal(mean=np.zeros((d,)),cov=np.eye(d)*dt)
+	def dW(self,dt):
+		return self.gen.multivariate_normal(mean=np.zeros((self.d,)),cov=np.eye(self.d)*dt)
 	
-	for ii in range(n):
-	
-		
-		xnot = gen.multivariate_normal(mean=np.zeros((d,)),cov=np.eye(d))
-		
-		
-		xx = [xnot]
-		for jj in range(1,len(t)+1):
-			
-			x = xx[jj-1]
-			tt = t[jj-1]
-			xx.append(x + f(x,tt)*dt + g(x,tt)@dW(dt))
-		
-		xx = np.vstack(xx)
-		assert xx.shape[0] == (len(t) + 1), print(xx.shape)
-		
-		allPaths.append(xx)
-	
-	
-	return allPaths
+	def init_conditions(self):
 
-def generate_stochastic_rossler(n=100,T=1,dt=0.001,coeffs=[0.1,0.1,14,0.,0.,0.],seed=1024):
+		return self.gen.multivariate_normal(mean=np.zeros((self.d,)),cov=np.eye(self.d))
+	
+class Rossler(ToyData):
 
-	"""
-	Makes a stochastic rossler attractor 
-	Takes as arguments:
-		n: number of trajectories to make
-		T: integration time
-		dt: integration timestep
-		coeffs:
-			[a,b,c,sd1,sd2,sd3]
-		where sd1-3 are dimension-wise sds of added noise
-		sigma: sd of noise added to rotation (default zero)
-		seed: seed for rng
-	
-	Returns:
-		trajectories:
-			list of n np.ndarrays of size (T/dt)x2, each element corresponding to a trajectory
-	"""    
-	
-	a,b,c = coeffs[0],coeffs[1],coeffs[2]
-	A = np.array([coeffs[3],coeffs[4],coeffs[5]])
-	Sig = np.eye(3) * A
-	t = np.arange(0,T,dt)
-	gen = np.random.default_rng(seed=seed)
-	
-	allPaths = [ ]
-	
-	def f(x,t):
+	def __init__(self,coeffs=[0.1,0.1,14],seed=1234):
+
+		super(Rossler,self).__init__()
+		self.a,self.b,self.c = coeffs
+		self.gen = np.random.default_rng(seed=seed)
+
+	def f(self,x,t):
 		dx = -x[1] - x[2] #+ sample_dW[0]
-		dy = x[0] + a*x[1] #+ sample_dW[1]
-		dz = b + x[2]*(x[0] - c) #+ sample_dW[2]
+		dy = x[0] + self.a*x[1] #+ sample_dW[1]
+		dz = self.b + x[2]*(x[0] - self.c) #+ sample_dW[2]
 		return np.hstack([dx,dy,dz])
 	
-	def g(x,t):
-		return Sig
+	def g(self,x,t,sigma):
+		return np.eye(3)*sigma
 	
-	def dW(dt):
-		return gen.multivariate_normal(mean=np.zeros((3,)),cov=np.eye(3)*dt)
+	def dW(self,dt):
+		return self.gen.multivariate_normal(mean=np.zeros((3,)),cov=np.eye(3)*dt)
 	
-	for ii in range(n):
-	
-		
-		xnot = gen.multivariate_normal(mean=np.zeros((3,)),cov=np.eye(3))
-		
-		
-		xx = [xnot]
-		
-		for jj in range(1,len(t)+1):
-			
-			x = xx[jj-1]
-			tt = t[jj-1]
+	def init_conditions(self):
 
-			xx.append(x + f(x,tt)*dt + g(x,tt)@dW(dt))
-		
-		xx= np.vstack(xx)
-		
-		assert xx.shape[0] == len(t) +1, print(xx.shape)
-		
-		allPaths.append(xx)
-	
-	
-	return allPaths
-	
-def generate_double_sde(n=100,T=1,dt=0.001,
-						omegas=(np.pi/5,-np.pi/2.5),centers=((-0.5,0),(0.5,0)),sigma=0.,
-						seed=1040):
-	"""
-	Uses the circle dynamics to create two opposing dynamical systems, separated at the x=0.
-	Takes as arguments:
-	n: number of trajectories to make
-	T: integration time
-	dt: integration timestep
-	omegas: rotation speed for (left,right) circles
-	sigma: sd of noise added to rotation (default zero)
-	centers: define center of each circle:
-	((left circle x, left circle y),
-	(right circle x, right circle y))
-	
-	Returns:
-	trajectories:
-	list of n np.ndarrays of size (T/dt)x2, each element corresponding to a trajectory
-	"""    
+		return self.gen.multivariate_normal(mean=[0,-9,0],cov=np.eye(3))
 
-	trajectories=[]
-	t = np.arange(0,T,dt)
-	gen = np.random.default_rng(seed=seed)
-	centers = (np.array(centers[0]),np.array(centers[1]))
-	
-	def f1(theta,t):
-		dtheta = omegas[0]*dt
-		return dtheta
-	def f2(theta,t):
-		dtheta = omegas[1]*dt
-		return dtheta
+class DoubleSDE(ToyData):
+
+	def __init__(self,coeffs=[np.pi,-np.pi,np.array([-0.5,0]),np.array([0.5,0])],seed=1234):
+
+		super(DoubleSDE,self).__init__()
+		self.omega1,self.omega2,self.center1,self.center2 = coeffs
+		#self.center2 = -self.center1
 		
-	def g(x,t):
+		self.gen = np.random.default_rng(seed=seed)
+
+	def ft(self,theta,x,t):
+		if x[0] < 0:
+			return self.omega1
+		else:
+			return self.omega2
+		
+	def g(self,x,t,sigma):
 		return sigma * np.eye(2)#/(np.abs(x[0])+1/10)
 	
-	def dW(dt):
-		return gen.normal(loc=np.zeros((2,)),scale=np.sqrt(dt))
+	def dW(self,dt):
+		return self.gen.multivariate_normal(mean=np.zeros((2,)),cov=np.eye(2)*dt)
 	
-	def polar_to_cartesian(r,theta):
+	def _polar_to_cartesian(self,r,theta):
 	
 		return np.hstack([r*np.cos(theta),r*np.sin(theta)])
 	
-	def cartesian_to_polar(xy):
+	def _cartesian_to_polar(self,xy):
 	
 		r = np.linalg.norm(xy)
-		theta = np.arctan(xy[1]/xy[0]) + np.pi * (xy[0] < 0)
+		theta = np.arctan2(xy[1],xy[0])
 		return r,theta
-
-	for ii in range(n):
-		
-		xnot = gen.normal(loc=[0,-0.5],scale=0.25)
-		
-		xx = [xnot]
-		
-		for jj in range(1,len(t)+1):
-			x = xx[jj-1]
-			tt = t[jj-1]
-
-			if x[0] < 0:
-				v = x - centers[0]
-				r,theta = cartesian_to_polar(v)
-				dtheta = f1(theta,tt)
-				theta += dtheta
-				xx2 = polar_to_cartesian(r,theta) + centers[0]
-			else:
-				v = x - centers[1]
-				r,theta = cartesian_to_polar(v)
-				dtheta = f2(theta,tt)
-				theta += dtheta
-				
-				xx2 = polar_to_cartesian(r,theta) + centers[1]
-			#print(xx2.shape)
-			#g=g(x,tt)
-			#dw = dW(dt)
-			#print(g.shape)
-			#print(dw.shape)
-			#print((g@dw).shape)
-   
-			xx.append(xx2 + g(x,tt)@dW(dt))
-		xx = np.vstack(xx)
-		
-		trajectories.append(xx)
 	
-	
-	return trajectories
+	def init_conditions(self):
 
-def generate_OU_balls(n=100,T=1,dt=0.001,imshape=(64,64),radius=2,
-						coeffs =np.array([-4,-4]),
-						sigma=0., 
-						seed=1040):
-	
+		return self.gen.multivariate_normal(mean=[0,-0.5],cov=np.eye(2)*0.01)
 
-	trajectories=[]
-	t = np.arange(0,T,dt)
-	gen = np.random.default_rng(seed=seed)
-	centers = (np.array(centers[0]),np.array(centers[1]))
+	def dx(self,x,t,dt,sigma):
 
-	def f(x,t):
+		if x[0] < 0:
+			v = x - self.center1
+			r,theta = self._cartesian_to_polar(v)
+			dtheta = self.ft(theta,x,t)
+			theta += dtheta*dt
+			xx2 = self._polar_to_cartesian(r,theta) + self.center1
+		else:
+			v = x - self.center2
+			r,theta = self._cartesian_to_polar(v)
+			dtheta = self.ft(theta,x,t)
+			theta += dtheta*dt
+			xx2 = self._polar_to_cartesian(r,theta) + self.center2
 
-		return coeffs @ x 
-	
-	def g(x,t):
+		return xx2 + self.g(x,t,sigma)@self.dW(dt) - x
+
+class Balls(ToyData):
+
+	def __init__(self,coeffs=[np.pi,-np.pi,np.array([-0.5,0]),np.array([0.5,0])],seed=1234):
+
+		super(Balls,self).__init__()
+		self.coeffs = coeffs
+
+		#self.center2 = -self.center1
+		
+		self.gen = np.random.default_rng(seed=seed)
+
+	def f(self,x,t):
+
+		return self.coeffs @ x 
+
+	def g(self,x,t,sigma):
 
 		return sigma*np.eye(2)
-	
-	def dW(dt):
 
-		return gen.multivariate_normal(mean=np.zeros((2,)),cov=dt*np.eye(2))
-	
+	def dW(self,dt):
 
-	for ii in range(n):
+		return self.gen.multivariate_normal(mean=np.zeros((2,)),cov=dt*np.eye(2))
 	
-		xnot = gen.normal(loc=[0,0],scale=0.25)
+	def init_conditions(self):
+
+		return self.gen.multivariate_normal(mean=[-1,0],cov=np.eye(2)*0.25**2)
+	
+	def traj_to_movie(self,trajectories,image_shape,radius,blur=False):
+		"""
+		converts a latent trajectory to a movie
+		"""
+
+
+		movies = []
+		gX,gY = np.meshgrid(np.linspace(-1,1,2*radius),np.linspace(-1,1,2*radius))
+		ball = (gX**2 + gY**2 < 1)
+		image_shape = np.array(image_shape)
 		
-		xx = [xnot]
+		for traj in trajectories:
+			traj -= np.amin(traj)
+			traj /= np.amax(np.abs(traj))
+
+			frames = np.zeros((len(traj),1,image_shape[0],image_shape[1]))
 		
-		for jj in range(1,len(t)+1):
+			for point in range(len(traj)):
+		
+				ballCenter = (traj[point,:] *(image_shape - 2*radius)).astype(int) + radius
+				frames[point,0,ballCenter[0]-radius:ballCenter[0] + radius, ballCenter[1]-radius:ballCenter[1] + radius] += ball
+				if blur:
+					frames[point,0,:,:] = gaussian_filter(frames[point,0,:,:],sigma=radius*4,truncate=0.05)
 
-			x = xx[jj-1]
-			tt = t[jj-1]
+			movies.append(frames)
 
-			xx.append(x + f(x,tt) + g(x,tt)@dW(dt))
+		return movies
 
-		xx = np.vstack(xx)
-		xx -= np.amin(xx)
-		xx /= np.amax(xx)
-
-		trajectories.append(xx)
-
-	movies = traj_to_movie(trajectories,imshape,radius)
-	
-	return trajectories,movies
-
-def traj_to_movie(trajectories,image_shape,radius):
-
-	"""
-	converts a latent trajectory to a movie
-	"""
-
-	movies = []
-	gX,gY = np.meshgrid(np.linaspace(-1,1,16),np.linspace(-1,1,16))
-	ball = (gX**2 + gY**2 < 1)
-
-	for traj in trajectories:
-		frames = np.zeros((len(traj),1,image_shape[0],image_shape[1]))
-
-		for point in range(len(traj)):
-
-			ballBound = (traj[point,:] *(image_shape - radius)).astype(int)
-			frames[point,0,ballBound[0]:ballBound[0] + ball.shape[0], ballBound[1]:ballBound[1] + ball.shape[1]] += ball 
-
-		movies.append(frames)
-	return movies
-	
 
 def downsample(data:list,origdt:float,newdt:float,noise:bool=True) -> np.ndarray:
 
